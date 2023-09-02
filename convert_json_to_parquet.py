f3f750575f22a700498d4bd806b66a1741e4b90d
jhchee <cheehong05090@gmail.com>
2023-09-02 12:31:18 +0800

    add object tags

--- convert_json_to_parquet.py
+++ convert_json_to_parquet.py
@@ -4,7 +4,7 @@ import os
 wr.config.s3_endpoint_url = 'http://localhost:9000'
 os.environ['AWS_SECRET_ACCESS_KEY'] = 'password'
 os.environ['AWS_ACCESS_KEY_ID'] = 'admin'
-
+tag_set = 'Key1=value1&Key2=value2'
 df = wr.s3.read_json(path="s3://warehouse/json_input/", orient="columns")
 df["date"] = df["timestamp"].dt.date
 df["hour"] = df["timestamp"].dt.hour
@@ -12,6 +12,7 @@ res = wr.s3.to_parquet(df=df,
                        path="s3://warehouse/parquet_output/",
                        pyarrow_additional_kwargs={"compression_level": 9},  # this is set to 1 by default
                        dataset=True,
+                       s3_additional_kwargs={'Tagging': tag_set},
                        compression="zstd",
                        partition_cols=["date", "hour"],
                        mode="overwrite"
